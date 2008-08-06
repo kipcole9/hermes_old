5e1fb2cefbac034190edd61abb398e6db1e54f48
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2008-08-06 15:23:28 +1000

    Handle URI error more gracefully

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -9,12 +9,19 @@ class Bookmark < ActiveRecord::Base
   before_save :check_url_and_title
 
 private
-    def check_url_and_title
-    response = Net::HTTP.get_response URI.parse(self.url)
-    puts "Response code for #{self.url} is #{response.code}"
+  def check_url_and_title
+    begin
+      response = Net::HTTP.get_response URI.parse(self.url)
+    rescue URI::InvalidURIError
+      logger.error "Bookmark: Net::HTTP thinks '#{self.url}' is a bad url"
+      self.errors.add("url", "appears to be bad")
+      return false
+    end
+    
     if response.code != "200" then
       return false
     end
+    
     # Update our title to the title of the page
     body = Hpricot(response.body)
     title = (body/"title").inner_html
