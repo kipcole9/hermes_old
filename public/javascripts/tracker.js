8e6f023bbd5e170d4640d4ecfac7e4ab60bb6781
Kip Cole <kip@kip-macbook.refuge.noexpectations.com.au>
2009-05-07 13:44:39 +1000

    Added vietools analytics feed to layout

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -1,5 +1,5 @@
 function _tks(account)  {
-	this.version		= "0.5";
+	this.version		= "0.6";
 	var self = this;
 	this.account 		= "";
 	this.trackerHost	= "noexpectations.com.au";
@@ -21,7 +21,7 @@ function _tks(account)  {
 			// Have to separate the next two functions
 			// to work in IE7.  Sigh.
 			f = this.urlParams[p]; value = f();
-			if (value != 'undefined') params[i++] = p + '=' + value;
+			if (value != 'undefined' && value && value.length > 0) params[i++] = p + '=' + value;
 		};
 		url = url + params.join('&');
 		url = url + "&uver=" + this.version;
@@ -36,6 +36,23 @@ function _tks(account)  {
 	};
 	this.getLanguage = function() {
 		return (navigator.language) ? navigator.language : navigator.userLanguage; 
+	};
+	this.getCharset = function() {
+		function getMetaHttpEquiv(){
+			var m = document.getElementsByTagName('meta'); 
+		  	for(var i in m){ 
+		   		if(m[i].httpEquiv && m[i].httpEquiv.match(/content-type/i)) { 
+		     		return m[i].content; 
+		   		}; 
+		  	};
+			return;
+		};
+		httpEquiv = getMetaHttpEquiv()
+		if (httpEquiv) {
+			return httpEquiv.match(/charset=(.*)($|;)/)[1];
+		} else {
+			return;
+		}
 	};
 	this.getUserAgent = function() {
 		return navigator.userAgent;
@@ -301,6 +318,63 @@ function _tks(account)  {
 	this.setCampContent = function(name) {
 		campaignContent = name;
 	};
+	/*
+	File: Math.uuid.js
+	Version: 1.3
+	Change History:
+	  v1.0 - first release
+	  v1.1 - less code and 2x performance boost (by minimizing calls to Math.random())
+	  v1.2 - Add support for generating non-standard uuids of arbitrary length
+	  v1.3 - Fixed IE7 bug (can't use []'s to access string chars.  Thanks, Brian R.)
+	  v1.4 - Changed method to be "Math.uuid". Added support for radix argument.  Use module pattern for better encapsulation.
+
+	Latest version:   http://www.broofa.com/Tools/Math.uuid.js
+	Information:      http://www.broofa.com/blog/?p=151
+	Contact:          robert@broofa.com
+	----
+	Copyright (c) 2008, Robert Kieffer
+	All rights reserved.
+
+	Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+	    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+	    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer \
+		in the documentation and/or other materials provided with the distribution.
+	    * Neither the name of Robert Kieffer nor the names of its contributors may be used to endorse or promote products derived from 
+		this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+	BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+	SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	*/
+
+	/*
+	 * Generate a random uuid.
+	 *
+	 * USAGE: Math.uuid(length, radix)
+	 *   length - the desired number of characters
+	 *   radix  - the number of allowable values for each character.
+	 *
+	 * EXAMPLES:
+	 *   // No arguments  - returns RFC4122, version 4 ID
+	 *   >>> Math.uuid()
+	 *   "92329D39-6F5C-4520-ABFC-AAB64544E172"
+	 * 
+	 *   // One argument - returns ID of the specified length
+	 *   >>> Math.uuid(15)     // 15 character ID (default base=62)
+	 *   "VcydxgltxrVZSTV"
+	 *
+	 *   // Two arguments - returns ID of the specified length, and radix. (Radix must be <= 62)
+	 *   >>> Math.uuid(8, 2)  // 8 character ID (base=2)
+	 *   "01001010"
+	 *   >>> Math.uuid(8, 10) // 8 character ID (base=10)
+	 *   "47473046"
+	 *   >>> Math.uuid(8, 16) // 8 character ID (base=16)
+	 *   "098F4D35"
+	 */
 	this.getUuid = function(len, radix) {
 		var CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''); 
 	  	var chars = CHARS, uuid = [], rnd = Math.random;
@@ -342,12 +416,13 @@ function _tks(account)  {
 		);
 		return objURL;
 	};
+	// Constructor
 	this.account = account;
 	parameters = this.parseParameters();
 	this.urlParams = {
 		"utvis": this.getVisitor, "utses": this.getSession,
 		"utmdt": this.getPageTitle,	"utmsr": this.getScreenSize, "utmsc": this.getColorDepth, 
-		"utmul": this.getLanguage, "utmfl": this.getFlashVersion, "utmn": this.getUniqueRequest,
+		"utmul": this.getLanguage, "utmcs": this.getCharset, "utmfl": this.getFlashVersion, "utmn": this.getUniqueRequest,
 		"utm_campaign": this.getCampName, "utm_source": this.getCampSource,
 		"utm_medium": this.getCampMedium, "utm_content": this.getCampContent,
 		"utmp": this.getUrl
